7615acd6b6ee4224e191a4975fefa5f17213af98
Juan Ma <jmher31@hotmail.com>
2017-11-09 01:15:39 -0600

    Terminando operaciones
    Todas las operaciones de la calculadora

--- Proyecto Calculadora/js/app.js
+++ Proyecto Calculadora/js/app.js
@@ -1,8 +1,12 @@
 var calculadora ={
+  /*Variables de operacion*/
   primerNum: 0,
   antiguoNumero: 0,
   acumulado: 0,
+  acumuladoMD: 1,
   operadorActual:"",
+
+// NOTE: Funciones del objeto
 
   reducirTamanoTecla: function (elemento){
     elemento.style.borderRadius="40%"
@@ -26,9 +30,6 @@ var calculadora ={
       if (elemento.id=="punto" && document.getElementById("display").textContent.indexOf(".")== -1) {
         document.getElementById('display').textContent=valorDisplay+"."
       }
-      if (elemento.id=="dividido" || elemento.id=="por" || elemento.id=="menos" || elemento.id=="mas") {
-        //document.getElementById('display').textContent=""
-      }
       
   },
   limpiarPantalla: function (elemento){
@@ -36,9 +37,11 @@ var calculadora ={
       document.getElementById('display').textContent="0"
       primerNum=0
       this.acumulado=0
+      this.acumuladoMD=1
       operadorActual=""
     }
   },
+  // NOTE: Cambio el signo a -/+ de un numero
   escribirSigno: function(elemento){
     var valorDisplay= document.getElementById('display').textContent
     if (valorDisplay!= 0 && elemento.id=="sign" && document.getElementById("display").textContent.indexOf("-")== -1) {
@@ -49,7 +52,7 @@ var calculadora ={
       document.getElementById('display').textContent=signo;
     }
   },
-
+  // NOTE: mostrar en pantalla max 8 caracteres
   validarLongitud: function(elemento){
     var cadenaDisplay= document.getElementById('display').textContent
     var chimi= elemento.id
@@ -58,33 +61,59 @@ var calculadora ={
       }
   },
 
-  operacionSuma: function(elemento){
+  Operaciones: function(elemento){
+
     antiguoNumero= primerNum
-    if (elemento.id=="mas") {
-      this.acumulado = parseFloat(this.acumulado) + parseFloat(antiguoNumero)
-      document.getElementById('display').textContent=""
-      operadorActual= elemento.id
+    switch (elemento.id){
+      case "mas":
+        this.acumulado = parseFloat(this.acumulado) + parseFloat(antiguoNumero)
+        document.getElementById('display').textContent=""
+        operadorActual= elemento.id
+      break;
+
+      case "menos":
+        this.acumulado = parseFloat(antiguoNumero) - parseFloat(this.acumulado) 
+        document.getElementById('display').textContent=""
+        operadorActual= elemento.id
+      break;
+
+      case "por":
+        this.acumuladoMD = parseFloat(this.acumuladoMD)  * parseFloat(antiguoNumero)
+        document.getElementById('display').textContent=""
+        operadorActual= elemento.id
+      break;
+
+      case "dividido":
+        this.acumuladoMD = parseFloat(antiguoNumero)/ parseFloat(this.acumuladoMD) 
+        document.getElementById('display').textContent=""
+        operadorActual= elemento.id
+      break;
     }
-    if (elemento.id=="por") {
-      this.acumulado = (parseFloat(this.acumulado) +1) * parseFloat(antiguoNumero)
-      document.getElementById('display').textContent=""
-      operadorActual= elemento.id
-    }
+
   },
 
   signoIgual: function(elemento){
+
     if (elemento.id=="igual"){
-    var numuno= parseFloat(antiguoNumero)
-    var numerodos= this.acumulado
-    if (operadorActual=="mas") {
-      document.getElementById('display').textContent= numuno+numerodos
-    }
-    else if (operadorActual=="por") {
-      document.getElementById('display').textContent= numuno*numerodos
-    }
+      var numuno= parseFloat(antiguoNumero)
+      var numerodos= this.acumulado
+      var numerodosMD=this.acumuladoMD
+
+      if (operadorActual=="mas") {
+        document.getElementById('display').textContent= numuno+numerodos
+      }
+      else if (operadorActual=="menos") {
+        document.getElementById('display').textContent= numerodos - numuno
+      }
+      else if (operadorActual=="por") {
+        document.getElementById('display').textContent= numuno*numerodosMD
+      }
+      else if (operadorActual=="dividido") {
+        document.getElementById('display').textContent= numerodosMD/ numuno 
+      }
     }
   },
-
+// NOTE: Asignacion de eventos para luego operar las funciones del objeto
   asignarEventosTeclas:function(){
     tecla= document.querySelectorAll('.tecla')
     teclaNumeros= document.querySelectorAll("img[class=tecla]");
@@ -106,7 +135,7 @@ var calculadora ={
     calculadora.asignarNumeroEnPantalla(event.target);
     calculadora.limpiarPantalla(event.target);
     calculadora.escribirSigno(event.target);
-    calculadora.operacionSuma(event.target)
+    calculadora.Operaciones(event.target)
     calculadora.signoIgual(event.target)
     }
 
